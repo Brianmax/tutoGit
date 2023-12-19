a6ce1c4beba3b98ab59f0d6c84ad66cbeabae1c6
Brianmax <george.maxi@ucsp.edu.pe>
2023-12-19 11:44:00 -0500

    cambios

--- Ejemplo.java
+++ Ejemplo.java
@@ -1,6 +1,9 @@
 public class Ejemplo {
     public static void main(String[] args) {
         
+        System.out.println("Hola mundo");
+        System.out.println("Hola dese git");
+        System.out.println("hola");
     }
     
 }
